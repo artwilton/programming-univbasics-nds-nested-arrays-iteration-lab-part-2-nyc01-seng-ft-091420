2b93c1c02954bb533b3bbb285eeb1d59188a959a
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-07 16:23:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,8 @@ def find_min_in_nested_arrays(src)
   new_array = ["test"]
   row_index = 0
   while row_index < src.count do
+    element_index = 0
+    while src[]
     row_index += 1
   end
   p new_array.count
